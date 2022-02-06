32f170466ecd591b6d419bf7adbae9d210ac265d
costelinha <andregomesngr@gmail.com>
2022-02-06 15:02:47 -0500

    set up supertest library

--- tests/exemple.spec.ts
+++ tests/exemple.spec.ts
@@ -1,7 +1,10 @@
 import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST }:${process.env.PORT}`
 
 test.group('Example', () => {
-    test('assert sum', (assert) => {
-        assert.equal(2+2, 4)
+    test('it GET /', async () => {
+        supertest(BASE_URL).get('/').expect(200)
     })
 })
