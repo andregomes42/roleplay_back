98438bcc29414141d9c056967c958e7de5c8e4a8
costelinha <andregomesngr@gmail.com>
2022-02-12 19:03:44 -0500

    update tests

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -1,14 +1,14 @@
-import { UserFactory } from 'Database/factories'
+import { UserFactory } from 'Database/factories/user'
 import test from 'japa'
 import supertest from 'supertest'
+
 
 const BASE_URL = `http://${process.env.HOST }:${process.env.PORT}/api/v1`
 
 test.group('Users', () => {
     test('it POST /users', async (assert) => {
-        const user =  await UserFactory.create()
+        const user = await UserFactory.makeStubbed()
         const { body } = await supertest(BASE_URL).post('/users').send(user).expect(201)
-        console.log(body)  
 
         assert.equal(body.user.username, user.username)
     })
